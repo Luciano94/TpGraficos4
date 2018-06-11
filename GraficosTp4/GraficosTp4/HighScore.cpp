1f403a9f238d92a5fc01f7856b9b45a513dda95b
Unknown <lucho.elp40@gmail.com>
2018-06-11 20:44:22 -0300

    v1.0
    Add HighScore files
    
    -add HighScore.h
    -add HighScore.cpp

--- GraficosTp4/GraficosTp4/HighScore.cpp
+++ GraficosTp4/GraficosTp4/HighScore.cpp
@@ -1,1 +1,71 @@
+#include "HighScore.h"
 
+HighScore::HS::HS()
+{
+	_HS = new list<score>();
+}
+
+HighScore::HS::~HS()
+{
+	delete _HS;
+}
+
+void HighScore::HS::addHS(const int point, const string name)
+{
+	score add;
+	add.point = point;
+	add.name = name;
+	_HS->push_back(add);
+	_HS->sort();
+}
+
+void HighScore::HS::getMin(int &point, string &name)
+{
+	name = _HS->back().name;
+	point = _HS->back().point;
+}
+
+void HighScore::HS::getMax(int &point, string &name)
+{
+	name = _HS->front().name;
+	point = _HS->front().point;
+}
+
+int HighScore::HS::getCant()
+{
+	return _HS->size();
+}
+
+bool HighScore::HS::getScore(const int pos, int & point, string & name)
+{
+	if ((int)_HS->size() < pos)
+		return false;
+	list<score>::iterator it = _HS->begin();
+	advance(it, pos);
+	name = it->name;
+	point = it->point;
+	return true;
+}
+
+void HighScore::HS::clearScore()
+{
+	_HS->clear();
+}
+
+list<int>* HighScore::HS::getPoints()
+{
+	list<int>* listPoints = new list<int>();
+	for (list<score>::iterator it = _HS->begin(); it != _HS->end(); ++it) {
+		listPoints->push_back(it->point);
+	}
+	return listPoints;
+}
+
+list<string>* HighScore::HS::getNames()
+{
+	list<string>* listPoints = new list<string>();
+	for (list<score>::iterator it = _HS->begin(); it != _HS->end(); ++it) {
+		listPoints->push_back(it->name);
+	}
+	return listPoints;
+}
--- GraficosTp4/GraficosTp4/HighScore.h
+++ GraficosTp4/GraficosTp4/HighScore.h
@@ -1,1 +1,26 @@
+#include <list>
+#include <iostream>
+using namespace std;
+namespace HighScore {
+	class HS {
+		struct score
+		{
+			int point;
+			string name;
+			bool operator<(const score &rhs) const { return  point > rhs.point; }
+		};
+		list<score>* _HS;
+	public:
+		HS();
+		~HS();
+		void addHS(const int point, const string name);
+		void getMin(int &point, string &name);
+		void getMax(int &point, string &name);
+		int getCant();
+		bool getScore(const int pos, int &point, string &name);
+		void clearScore();
+		list<int>* getPoints();
+		list<string>* getNames();
+	};
+}
 
